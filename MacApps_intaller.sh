49856ab5ce6a484e7c5944f6f9342f550324c302
Tadeu Banzato <10622335+tadeubanzato@users.noreply.github.com>
2021-05-28 16:28:01 -0400

    Update MacApps_intaller.sh

--- MacApps_intaller.sh
+++ MacApps_intaller.sh
@@ -4,7 +4,7 @@
 ## Run command bellow will install Homebre on your Mac and all listed apps bellow
 ##
 ## To run the installer directly from Github
-## sudo curl -fsSL https://raw.githubusercontent.com/tadeubanzato/PersonalMacPrompt/main/MacApps_intaller.sh | bash
+## curl -fsSL https://raw.githubusercontent.com/tadeubanzato/PersonalMacPrompt/main/MacApps_intaller.sh | bash
 ##
 ## Homebrew Relevant Commands
 ## brew list > Will list all instlled packages
@@ -22,16 +22,16 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 ## Install Homebrew
-printf "\n\n ${YELLOW}--------> INSTALLING HOMEBREW ${NC}\n\n"
+printf "\n\n ${GREEN}--------> INSTALLING HOMEBREW ${NC}\n"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Terminal Tools
-printf "\n\n ${YELLOW}--------> INSTALLING TERMINAL TOOLS ${NC}\n\n"
+printf "\n\n ${GREEN}--------> INSTALLING TERMINAL TOOLS ${NC}\n"
 brew install zsh
 brew install --cask hyper
 
 # Dev Tools
-printf "\n\n ${YELLOW}--------> INSTALLING DEV TOOLS ${NC}\n\n"
+printf "\n\n ${GREEN}--------> INSTALLING DEV TOOLS ${NC}\n\n"
 brew install --cask github
 brew install --cask atom
 brew install --cask processing
@@ -40,7 +40,7 @@ brew install --cask vnc-viewer
 brew install --cask postman
 
 # System Tools
-printf "\n\n ${YELLOW}--------> INSTALLING SYSTEM APPS ${NC}\n\n"
+printf "\n\n ${GREEN}--------> INSTALLING SYSTEM APPS ${NC}\n\n"
 brew install --cask google-chrome
 brew install --cask appcleaner
 brew install --cask dropbox
@@ -51,17 +51,17 @@ brew install --cask zoom
 brew install --cask microsoft-office
 
 # Communication Tools
-printf "\n\n ${YELLOW}--------> INSTALLING COMMUNICATION APPS ${NC}\n\n"
+printf "\n\n ${GREEN}--------> INSTALLING COMMUNICATION APPS ${NC}\n\n"
 brew install --cask telegram
 brew install --cask whatsapp
 brew install --cask webex
 brew install --cask slack
 
 # Music and Video Tools
-printf "\n\n ${YELLOW}--------> INSTALLING MUSIC AND VIDEO APPS ${NC}\n\n"
+printf "\n\n ${GREEN}--------> INSTALLING MUSIC AND VIDEO APPS ${NC}\n\n"
 brew install --cask spotify
 brew install --cask vlc
 
 # Creative Tools
-printf "\n\n ${YELLOW}--------> INSTALLING CREATIVE TOOLS ${NC}\n\n"
+printf "\n\n ${GREEN}--------> INSTALLING CREATIVE TOOLS ${NC}\n\n"
 brew install --cask adobe-creative-cloud
