5465090915f732dff971f0069450bb06b6aef177
Tadeu Banzato <10622335+tadeubanzato@users.noreply.github.com>
2021-06-02 11:54:30 -0400

    Update MacApps_intaller.sh

--- MacApps_intaller.sh
+++ MacApps_intaller.sh
@@ -30,43 +30,66 @@ printf "\n\n ${GREEN}--------> INSTALLING HOMEBREW ${NC}\n"
 
 # Install Terminal Tools
 printf "\n\n ${GREEN}--------> INSTALLING TERMINAL TOOLS ${NC}\n"
+# ZSH https://www.zsh.org/
 brew install zsh
+# Hyper https://hyper.is/
 brew install --cask hyper
 
 # Dev Tools
 printf "\n\n ${GREEN}--------> INSTALLING DEV TOOLS ${NC}\n\n"
+# GitHub https://desktop.github.com/
 brew install --cask github
+# Atom https://atom.io/
 brew install --cask atom
+# Processing https://processing.org/download/
 brew install --cask processing
-brew install --cask sqlpro-for-sqlite
+# VNC Viewer https://www.realvnc.com/
 brew install --cask vnc-viewer
+# Postman https://www.postman.com/downloads/
 brew install --cask postman
+# DB Browser https://sqlitebrowser.org/
 brew install --cask db-browser-for-sqlite
 
 # System Tools
 printf "\n\n ${GREEN}--------> INSTALLING SYSTEM APPS ${NC}\n\n"
+# Google Chrome https://www.google.com/chrome/
 brew install --cask google-chrome
+# App Clearner https://freemacsoft.net/appcleaner/
 brew install --cask appcleaner
+# Dropbox https://www.dropbox.com/downloading
 brew install --cask dropbox
+# Evernote https://evernote.com/download/
 brew install --cask evernote
+# Discord https://discord.com/download
 brew install --cask discord
+# Unarchiver https://macpaw.com/the-unarchiver
 brew install --cask the-unarchiver
+# Zoom https://zoom.us/support/download
 brew install --cask zoom
+# Microsoft Office https://www.microsoft.com/en-us/microsoft-365/mac/microsoft-365-for-mac
 brew install --cask microsoft-office
 
 # Communication Tools
 printf "\n\n ${GREEN}--------> INSTALLING COMMUNICATION APPS ${NC}\n\n"
+# Telegram https://desktop.telegram.org/
 brew install --cask telegram
+# WhatsApp https://www.whatsapp.com/download
 brew install --cask whatsapp
+# Webex https://www.webex.com/downloads.html/
 brew install --cask webex
+# Slack https://slack.com/downloads/windows
 brew install --cask slack
 
 # Music and Video Tools
 printf "\n\n ${GREEN}--------> INSTALLING MUSIC AND VIDEO APPS ${NC}\n\n"
+# Spotify https://www.spotify.com/us/download/windows/
 brew install --cask spotify
+# VLC https://www.videolan.org/vlc/
 brew install --cask vlc
 
 # Creative and Dsign Tools
 printf "\n\n ${GREEN}--------> INSTALLING CREATIVE TOOLS ${NC}\n\n"
+# Adobe Creative Cloud https://creativecloud.adobe.com/apps/download/creative-cloud
 brew install --cask adobe-creative-cloud
+# Figma https://www.figma.com/downloads/
 brew install --cask figma
